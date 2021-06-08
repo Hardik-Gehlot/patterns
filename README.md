b3cbcf6ab247099f9a315ff194b9d03f65ae0d8a
Hardik Gehlot <hardikgehlot2303@gmail.com>
2021-06-08 12:00:05 +0530

    added numbers pyramid patterns and updated star pattern

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+```java
 package logical.Patterns;
 class Triangle{
     public void leftBottom(int num){
@@ -485,3 +486,157 @@ public class Star_Pattern {
     public static void main (String[] args) {
     }
 }
+```
+```java
+package logical.Patterns;
+class NumberPyramid{
+    public void bottom(int num){
+        /*
+           1
+          2 2
+         3 3 3
+        4 4 4 4
+         */
+        for(int row=1; row<=num; row++){
+            for(int colBlank=1; colBlank<=num-row; colBlank++){
+                System.out.print (" ");
+            }
+            for(int colFill=1; colFill<=row; colFill++){
+                if(colFill==row){
+                    System.out.print (row);
+                }
+                else{
+                    System.out.print (row+" ");
+                }
+            }
+            System.out.println ("");
+        }
+    }
+    public void bottom2(int num){
+        /*
+             0
+            101
+           21012
+          3210123
+         432101234
+        54321012345
+         */
+        for(int row=0; row<=num; row++){
+            for(int colBlank=1; colBlank<=num-row; colBlank++){
+                System.out.print (" ");
+            }
+            for(int colFill=row; colFill>=0; colFill--){
+                System.out.print (colFill);
+            }
+            for(int colFill=1;colFill<=row;colFill++){
+                System.out.print (colFill);
+            }
+            System.out.println ("");
+        }
+    }
+    public void top1a(int num){
+        /*
+            5 4 3 2 1
+             4 3 2 1
+              3 2 1
+               2 1
+                1
+         */
+        for(int row=1;row<=num; row++){
+            for(int colBlank=1; colBlank<=row-1; colBlank++){
+                System.out.print (" ");
+            }
+            for(int colFill=num-row+1; colFill>=1; colFill--){
+                if(colFill==1){
+                    System.out.print (colFill);
+                }
+                else{
+                    System.out.print (colFill+" ");
+                }
+            }
+            System.out.println ("");
+        }
+    }
+    public void top1b(int num){
+        /*
+            5 5 5 5 5
+             4 4 4 4
+              3 3 3
+               2 2
+                1
+    In line number 90 and 93 if we change num-row+1 into row then it will print
+               1 1 1 1 1
+                2 2 2 2
+                 3 3 3
+                  4 4
+                   5
+         */
+        for(int row=1;row<=num; row++){
+            for(int colBlank=1; colBlank<=row-1; colBlank++){
+                System.out.print (" ");
+            }
+            for(int colFill=num-row+1; colFill>=1; colFill--){
+                if(colFill==1){
+                    System.out.print (num-row+1);
+                }
+                else{
+                    System.out.print (num-row+1+" ");
+                }
+            }
+            System.out.println ("");
+        }
+    }
+    public void top2(int num){
+        /*
+        54321012345
+         432101234
+          3210123
+           21012
+            101
+             0
+         */
+        for(int row=0;row<=num; row++){
+            for(int colBlank=1; colBlank<=row; colBlank++){
+                System.out.print (" ");
+            }
+            for(int colFill=num-row; colFill>=0; colFill--){
+                System.out.print (colFill);
+            }
+            for(int colFill=1; colFill<=num-row; colFill++){
+                System.out.print (colFill);
+            }
+            System.out.println ("");
+        }
+    }
+    public void rhombus(int num){
+        /*
+                0
+              1 0 1
+            2 1 0 1 2
+          3 2 1 0 1 2 3
+        4 3 2 1 0 1 2 3 4
+          3 2 1 0 1 2 3
+            2 1 0 1 2
+              1 0 1
+                0
+         */
+        bottom2 (num);
+        for(int row=1;row<=num; row++){
+            for(int colBlank=1; colBlank<=row; colBlank++){
+                System.out.print (" ");
+            }
+            for(int colFill=num-row; colFill>=0; colFill--){
+                System.out.print (colFill);
+            }
+            for(int colFill=1; colFill<=num-row; colFill++){
+                System.out.print (colFill);
+            }
+            System.out.println ("");
+        }
+    }
+}
+public class NumberPatterns {
+    public static void main (String[] args) {
+    }
+}
+```
--- Star_Pattern.java
+++ Star_Pattern.java
@@ -481,6 +481,198 @@ class Line{
 
     }
 }
+class HollowTriangle{
+    public void leftBottom(int num){
+        /*
+         *
+         * *
+         *   *
+         *     *
+         * * * * *
+         */
+        for(int row=1; row<=num; row++){
+            if(row!=num){
+                for(int col=1; col<=row; col++){
+                    if(col==1 || col==row){
+                        System.out.print ("* ");
+                    }
+                    else{
+                        System.out.print ("  ");
+                    }
+                }
+            }
+            else{
+                for(int col=1; col<=row; col++){
+                    System.out.print ("* ");
+                }
+            }
+            System.out.println ("");
+        }
+    }
+    public void leftTop(int num){
+        /*
+         * * * * *
+         *     *
+         *   *
+         * *
+         *
+         */
+        for(int row=1; row<=num; row++){
+            if(row!=1){
+                for(int col=1; col<=num+1-row; col++){
+                    if(col==1 || col==num+1-row){
+                        System.out.print ("* ");
+                    }
+                    else{
+                        System.out.print ("  ");
+                    }
+                }
+            }
+            else{
+                for(int col=1; col<=num; col++){
+                    System.out.print ("* ");
+                }
+            }
+            System.out.println ("");
+        }
+    }
+    public void rightBottom(int num){
+        /*
+               *
+             * *
+           *   *
+         *     *
+       * * * * *
+         */
+        for(int row=1; row<=num; row++){
+            if(row!=num){
+                for(int colBlank=1; colBlank<=num-row; colBlank++){
+                    System.out.print ("  ");
+                }
+                for(int colFill=1; colFill<=row; colFill++){
+                    if(colFill==1 || colFill==row){
+                        System.out.print ("* ");
+                    }
+                    else {
+                        System.out.print ("  ");
+                    }
+
+                }
+            }
+            else{
+                for(int colFill=1; colFill<=row; colFill++){
+                    System.out.print ("* ");
+                }
+            }
+            System.out.println ("");
+        }
+    }
+    public void rightTop(int num){
+        /*
+         * * * * *
+           *     *
+             *   *
+               * *
+                 *
+         */
+        for(int row=1; row<=num; row++){
+            if(row!=1){
+                for(int colBlank=1; colBlank<=row-1; colBlank++){
+                    System.out.print ("  ");
+                }
+                for(int colFill=num; colFill>=row; colFill--){
+                    if(colFill==num || colFill==row){
+                        System.out.print ("* ");
+                    }
+                    else {
+                        System.out.print ("  ");
+                    }
+                }
+            }
+            else{
+                for(int colFill=num; colFill>=row; colFill--){
+                    System.out.print ("* ");
+                }
+            }
+            System.out.println ("");
+        }
+    }
+    public void leftBottomTop(int num){
+        /*
+         *
+         * *
+         *   *
+         *     *
+         *       *
+         *     *
+         *   *
+         * *
+         *
+         */
+        for(int row=1; row<=num; row++){
+            for(int col=1; col<=row; col++){
+                if(col==1 || col==row){
+                    System.out.print ("* ");
+                }
+                else{
+                    System.out.print ("  ");
+                }
+            }
+            System.out.println ("");
+        }
+        for(int row=1; row<=num-1; row++){
+            for(int col=1; col<=num-row; col++){
+                if(col==1 || col==num-row){
+                    System.out.print ("* ");
+                }
+                else{
+                    System.out.print ("  ");
+                }
+            }
+            System.out.println ("");
+        }
+
+    }
+    public void rightBottomTop(int num){
+        /*
+               *
+             * *
+           *   *
+         *     *
+           *   *
+             * *
+               *
+         */
+        for(int row=1; row<=num; row++){
+            for(int colBlank=1; colBlank<=num-row; colBlank++){
+                System.out.print ("  ");
+            }
+            for(int colFill=1; colFill<=row; colFill++){
+                if(colFill==1 || colFill==row){
+                    System.out.print ("* ");
+                }
+                else {
+                    System.out.print ("  ");
+                }
+            }
+            System.out.println ("");
+        }
+        for(int row=2; row<=num; row++){
+              for(int colBlank=1; colBlank<=row-1; colBlank++){
+                  System.out.print ("  ");
+              }
+              for(int colFill=num; colFill>=row; colFill--){
+                  if(colFill==num || colFill==row){
+                      System.out.print ("* ");
+                  }
+                  else {
+                      System.out.print ("  ");
+                  }
+              }
+            System.out.println ("");
+        }
+    }
+}
 public class Star_Pattern {
     public static void main (String[] args) {
     }
